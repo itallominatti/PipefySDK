e234ce85526db5532429b67a8f5406f4dd979b02
itallominatti <itallominatti@gmail.com>
2025-03-19 13:32:12 -0300

    [ main ] -> feat: enhance GraphQL queries with new search functionality and add request model

--- src/pipefysdk/queries/query_cards.py
+++ src/pipefysdk/queries/query_cards.py
@@ -1,21 +1,63 @@
 class GraphQLQueries:
-    
     @staticmethod
-    def search_fields_in_card(card_id: int):
+    def search_fields_in_card(card_id: int) -> str:
+        query = f"""
+        {{
+          card(id: "{card_id}") {{
+            fields {{
+              field {{
+                id
+              }}
+              name
+              value
+            }}
+          }}
+        }}
         """
+        return query
+
+    @staticmethod
+    def value_in_field_exists(pipe_id: int, field_id: str, value: str) -> str:
+        """
+        Check if a value exists in a field on a pipe.
+
+        Args:
+            pipe_id (int): ID of the pipe
+            field_id (str): ID of the field
+            value (str): Value to check
+
+        Returns:
+            str: GraphQL query string
         """
         query = f"""
-            {{
-                card(id: "{card_id}") {{
-                    fields {{
-                        field {{
-                            id
-                        }}
-                        name
-                        value
-                    }}
+        query {{
+          findCards(
+            pipeId: {pipe_id}
+            search: {{ fieldId: "{field_id}", fieldValue: "{value}" }}
+          ) {{
+            edges {{
+              node {{
+                fields {{
+                  date_value
+                  datetime_value
+                  filled_at
+                  float_value
+                  indexName
+                  name
+                  native_value
+                  report_value
+                  updated_at
+                  value
                 }}
+                title
+                id
+                current_phase {{
+                  id
+                }}
+                expired
+                createdAt
+              }}
             }}
-
-        """
+          }}
+        }}"""
         return query
