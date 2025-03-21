6ac96528aa72ed73dd41cef28397ce75789ec7f9
itallominatti <itallominatti@gmail.com>
2025-03-21 15:55:14 -0300

    [ main ] refactor: update import path for PipefySDK and remove main execution block

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,3 @@
-from pipefysdk.main import PipefySDK
+from src.main import PipefySDK
 
 __all__ = ["PipefySDK"]
-
-if __name__ == "__main__":
-    pipefy_sdk = PipefySDK(token="eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3MzcwNDYxNTUsImp0aSI6ImNjOTlkM2U3LWRkOTUtNDc2Yi1hYmIwLWYxMjgyZDFlYjkzZSIsInN1YiI6MjI2OTA3LCJ1c2VyIjp7ImlkIjoyMjY5MDcsImVtYWlsIjoiYW5hbHl0aWNzLmlubm92YXRpb24uc3BAYWNjZW50dXJlLmNvbSJ9fQ.KbsiNDodOr6jryfGoPwpi28idutqiVuzp7ZW3P79YNmHCYIPzGJMc2O2DraHfn3mwIdZFiRsTWeXUllj5wsKHw",
-        url="https://telefonicalogistica.pipefy.com/graphql")
-
-    get_card_info = pipefy_sdk.get_card_info(card_id=465540876)
-    print(get_card_info)
